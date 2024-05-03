2291ef9e83fe31e95b72c5332e84d1a7a34044ef
Derek Anderson <derekanderson.business@gmail.com>
2024-05-02 23:29:50 -0700

    Modified scripts to examine object closer to device camera

--- Assets/Scripts/Examinable.cs
+++ Assets/Scripts/Examinable.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Examinable : MonoBehaviour
 {
+    [SerializeField]
+    private ExaminableManager _examinableManager;
+
     // Start is called before the first frame update
     void Start()
     {
+        _examinableManager = FindObjectOfType<ExaminableManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("test!");
+
     }
+
+    //Request object to be examined when selected
+    public void RequestExamine()
+    {
+        _examinableManager.PerformExamine(this);
+        print("Examine has been requested");
+    }
+
 }
--- Assets/Scripts/ExaminableManager.cs
+++ Assets/Scripts/ExaminableManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ExaminableManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private Transform _examineTarget;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PerformExamine(Examinable examinable)
+    {
+        examinable.transform.position = _examineTarget.position;
+        examinable.transform.parent = _examineTarget;
     }
 }
